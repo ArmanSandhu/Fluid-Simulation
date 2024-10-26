bbefa9bd07859e887fc4c1499b149c66d7629e34
Arman Sandhu <armanssandhu@gmail.com>
2024-10-25 19:59:08 -0700

    Added Particle class and updated Simulation to draw particles.

--- src/simulation.js
+++ src/simulation.js
@@ -1,6 +1,23 @@
 class Simulation {
     constructor() {
+        this.particles = [];
+        this.PARTICLE_COUNT = 1000;
+        this.VELOCITY_DAMPING = 1;
 
+        this.initializeParticles();
+    }
+
+    initializeParticles() {
+        let particleOffset = 20;
+        let globalOffset = new Vector2(750, 100);
+        let currParticleCount = Math.sqrt(this.PARTICLE_COUNT);
+
+        for (let x = 0; x < currParticleCount; x++) {
+            for (let y = 0; y < currParticleCount; y++) {
+                let currPosition = new Vector2(x * particleOffset + globalOffset.x, y * particleOffset + globalOffset.y);
+                this.particles.push(new Particle(currPosition));
+            }
+        }
     }
 
     update(deltaTime) {
@@ -8,6 +25,29 @@ class Simulation {
     }
 
     draw(){
-        
+        for (let i = 0; i < this.particles.length; i++) {
+            let position = this.particles[i].position;
+            let color = this.particles[i].color;
+            DrawUtils.drawPoint(position, 5, color);
+        }
+    }
+
+    // This method loops through all the particles and saves their previous positions and updates the next postion.
+    predictPositions(deltaTime) {
+        for(let i = 0; i < this.particles.length; i++) {
+            // Save the previous position
+            this.particles[i].prevPosition = this.particles[i].position.Cpy();
+            let nextPosition = Scale(this.particles[i].velocity, deltaTime * this.VELOCITY_DAMPING);
+            // Advance to the next possible position
+            this.particles[i].position = Add(this.particles[i].position, )
+        }
+    }
+
+    // This method loops through all the particles and predicts their new velocity.
+    computeNextVelocity(deltaTime) {
+        for (let i = 0; i < this.particles.length; i++) {
+            let velocity = Scale(Sub(this.particles[i].position, this.particles[i].prevPosition), 1.0 / deltaTime);
+            this.particles[i].velocity = velocity;
+        }
     }
 }
